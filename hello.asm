8af83e42a3a4b72bbb08fd963a54c7c9e2bd4d1f
Jur Van toledo <jvan-tol@f1r6s12.codam.nl>
2023-11-15 16:49:23 +0100

    Added a helper.txt file for when i forget lol

--- hello.asm
+++ hello.asm
@@ -1,16 +1,22 @@
 section .text
-    global _start
+    global _start ;must be declared for linker (ld)
 
-_start:
-    mov edx,len
-    mov ecx,msg
-    mov ebx,1
-    mov eax,4
-    int 0x80
+_start: ;tells linker entry point
+    mov edx,len ;message length
+    mov ecx,msg ;message to write
+    mov ebx,1 ;file descriptor (stdout)
+    mov eax,4 ;system call number (sys_write) -> sys_write = 4
+    int 0x80 ;call kernel
 
-    mov eax,1
-    int 0x80
+    mov eax,1 ;system call number (sys_exit) -> sys_exit = 1
+    int 0x80 ;call kernel
 
     section .data
     msg db 'Hello, world!', 0xa
     len equ $ - msg
+
+// For compiling:
+  // To assemble the program, type nasm -f elf hello.asm
+  // if no error hello.o 
+  // To link the object file and create an executable file named hello, type ld -m elf_i386 -s -o hello hello.o
+  // Execute the program by typing ./hello
